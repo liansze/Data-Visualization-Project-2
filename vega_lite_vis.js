f72e9a61ed1fb597dd3cfc707ec58a1f85b77b5b
Jocelyn Tan <jtan0359@student.monash.edu>
2024-09-15 08:40:57 +0800

    add area chart

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -2,3 +2,8 @@ var vg_1 = "my.json";
 vegaEmbed("#map", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var vg_2 = "penetration_rate.json";
+vegaEmbed("#area", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
