fdcc611f2ef030b90ea66696dc22dda58be13c4c
Jocelyn Tan <jtan0359@student.monash.edu>
2024-10-10 07:43:38 +0800

    draft for visualizations

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,9 +1,19 @@
 var vg_1 = "my.json";
-vegaEmbed("#map", vg_1).then(function(result) {
+vegaEmbed("#map", vg_1, { "actions": false }).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_2 = "penetration_rate.json";
-vegaEmbed("#area", vg_2).then(function(result) {
+var vg_4 = "bump.json";
+vegaEmbed("#bump", vg_4, { "actions": false }).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_4 = "asean";
+vegaEmbed("#asean", vg_4, { "actions": false }).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_5 = "heatmap";
+vegaEmbed("#heatmap", vg_5, { "actions": false }).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
